59a1423d38fcdde62746b9f79cc36dd1651067e7
konekato <18ec033@ms.dendai.ac.jp>
2020-04-08 00:47:44 +0900

    add: func stairs_number_locker

--- cryptographism/number.py
+++ cryptographism/number.py
@@ -25,3 +25,20 @@ def mono_number_unlocker(cipher, key):
         plain += str(plain_num)
 
     return plain
+
+# stairs_number_... -> key + 0,1,2,3...9 を引いて暗号作成
+def stairs_number_locker(plain, key):
+    plain_array = list(map(int, str(plain)))
+    cipher = ""
+
+    for p in plain_array:
+        cipher_num = p - key
+        if cipher_num < 0:
+            cipher_num += 10
+        cipher += str(cipher_num)
+
+        key += 1
+        if key > 9:
+            key = 0
+
+    return cipher
